fc40358ee9e5f18743a7ce121d54b044768b1411
gabar0110 <146788836+gabar0110@users.noreply.github.com>
2024-06-12 16:51:40 -0300

    Add files via upload

---
+++ aula12.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import speech_recognition as sr
+
+st.title("transcrição")
+upload = st.file_uploader("faça upload",type=["wav"])
+if upload  is not None:
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(arquivo) as source:
+        st.write("processando audio...")
+        audio = recognizer.record(source)
+        texto = recognizer.recognize_google(audio,language="pt-BR")
+        st.write("Texto:",texto)
